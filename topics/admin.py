7762c35fa556fc6e6c12441fca33504455a08c1b
Serhii Slipchenko <sliptchenko71@gmail.com>
2021-11-07 15:55:57 +0200

    configure admin

--- topics/admin.py
+++ topics/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Student, ScientificDirector, BachelorTopic
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ('first_name', 'last_name')
+
+
+@admin.register(ScientificDirector)
+class ScientificDirectorAdmin(admin.ModelAdmin):
+    search_fields = ('first_name', 'last_name')
+
+
+@admin.register(BachelorTopic)
+class BachelorTopicAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'year', )
+
+    list_display = ('title', 'students', 'director', 'year')
+    ordering = ('title', 'year', 'director', )
+
+    def students(self, obj):
+        return ", ".join(str(student) for student in obj.student_set.all())
