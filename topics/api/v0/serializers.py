7faa9078da01a194943a4885077988124e68ec0a
Serhii Slipchenko <sliptchenko71@gmail.com>
2021-11-07 19:52:57 +0200

    add serializers for ScientificDirector and Student

--- topics/api/v0/serializers.py
+++ topics/api/v0/serializers.py
@@ -1,5 +1,19 @@
 from rest_framework import serializers
 
 
+class ScientificDirectorSerializer(serializers.Serializer):
+    pk = serializers.IntegerField()
+    last_name = serializers.CharField(max_length=200)
+    first_name = serializers.CharField(max_length=200)
+    patronymic = serializers.CharField(max_length=200)
+
+
+class StudentSerializer(serializers.Serializer):
+    pk = serializers.IntegerField()
+    last_name = serializers.CharField(max_length=200)
+    first_name = serializers.CharField(max_length=200)
+    patronymic = serializers.CharField(max_length=200)
+
+
 class BachelorTopicSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=200)
