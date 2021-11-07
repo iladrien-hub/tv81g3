c705ca0f0b6ddd50440af0379279002190f70c74
Serhii Slipchenko <sliptchenko71@gmail.com>
2021-11-07 19:20:52 +0200

    create url for ListTopics view

--- topics/api/v0/urls.py
+++ topics/api/v0/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from topics.api.v0.views import ping
+from topics.api.v0.views import ping, ListTopics
 
 app_name = 'topics'
 
 urlpatterns = [
     path('', ping, name='ping'),
+
+    path('topics/', ListTopics.as_view(), name="topics")
 ]
