0d5996d239aa505416f55029c06566617e763420
Serhii Slipchenko <sliptchenko71@gmail.com>
2021-11-07 19:20:31 +0200

    create ListTopics view

--- topics/api/v0/views.py
+++ topics/api/v0/views.py
@@ -1,6 +1,11 @@
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from topics.api.v0.serializers import BachelorTopicSerializer
+from topics.models import BachelorTopic
 
 
 @api_view(('GET',))
@@ -9,3 +14,15 @@ def ping(request):
     return Response(data={
         "message": "Hi there! Topics APIv0 up and running!"
     })
+
+
+class ListTopics(ListAPIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    permission_classes = [AllowAny]
+    queryset = BachelorTopic.objects.all()
+    serializer_class = BachelorTopicSerializer
